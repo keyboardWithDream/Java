86cc0b993b86ba6d0e0b266bd45517c60cffe092
KeyboardWithDream <isharlan.hu@gmail.com>
2020-06-07 22:55:41 +0800

    字符流

---
+++ IO流/4.四个抽象类/src/char_input/CharInput.java
@@ -0,0 +1,44 @@
+package char_input;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * 文件字符输入流
+ * 1.创建源
+ * 2.选择流
+ * 3.操作
+ * 4.释放资源
+ * @Author: Harlan
+ * @Date: 2020/6/7 22:35
+ */
+public class CharInput {
+
+    public static void  main(String[] args) {
+        File file = new File("./4.四个抽象类/src/input.txt");
+        FileReader reader = null;
+        try{
+            reader = new FileReader(file);
+            char[] flush = new char[1024];
+            int len = -1;
+            while ((len = reader.read(flush)) != -1){
+                String str = new String(flush,0,len);
+                System.out.println(str);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ IO流/4.四个抽象类/src/char_output/CharOutput.java
@@ -0,0 +1,37 @@
+package char_output;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * 文件字符输出流
+ * @Author: Harlan
+ * @Date: 2020/6/7 22:44
+ */
+public class CharOutput {
+
+    public static void main(String[] args) {
+        File file = new File("./4.四个抽象类/src/output.txt");
+
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(file,true);
+            String str = "你好! 世界!\r\n你好! 加瓦!\r\n你好! 号南\r\n";
+            // 字符串 ---> 字符数组
+            char[] data = str.toCharArray();
+            writer.write(str);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (writer != null){
+                try{
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
